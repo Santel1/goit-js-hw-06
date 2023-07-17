c5c098fec1671cbb52d9906256373607b9961535
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-07-17 10:47:02 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const inputEl = document.querySelector(`input[data-length="6"]`);
 inputEl.addEventListener("blur", (event) => {
   const length = event.currentTarget.value.length;
 
-  if (length <= inputEl.dataset.length) {
+  if (length < inputEl.dataset.length) {
     inputEl.classList.remove("valid");
     inputEl.classList.add("invalid");
   } else {
